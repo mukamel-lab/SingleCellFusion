e00a4d2d80e6c0ab71bcc90db58392cfcd8060f4
Fangming Xie <fmxie1993@gmail.com>
2021-02-21 23:11:09 -0800

    subsample_and_cv

--- scripts/SCF_main_repeat_subsampling_subset_features.py
+++ scripts/SCF_main_repeat_subsampling_subset_features.py
@@ -12,6 +12,7 @@ import argparse
 
 import basic_utils
 import SCF_utils
+import cluster_cv_utils
 
 
 def create_parser():
@@ -21,6 +22,9 @@ def create_parser():
     parser.add_argument("-c", "--config_py", help="Configuration file", required=True)
     parser.add_argument("-s", "--subsample_fraction", help="0~1", type=float, required=True)
     parser.add_argument("-sn", "--subsample_times", help=">1", type=int, required=True)
+    parser.add_argument("-fs", "--feature_subsample_fraction", help="0~1", type=float, required=True)
+    parser.add_argument("-fsn", "--feature_subsample_times", help=">1", type=int, required=True)
+    parser.add_argument("-ga", "--gene_annotation", help="gene annotation file (.bed)", type=str, required=True)
     return parser
 
 def subsampling(mods_selected, settings, metas, gxc_hvftrs, p=0, n=0):
@@ -79,8 +83,14 @@ if not os.path.isdir(outdir):
     os.makedirs(outdir)
 # end of configurations
 
+# get gene chrom lookup table (Dec 29, 2020)
+gene_chrom_lookup = pd.read_csv(args.gene_annotation, sep='\t', header=None)[[0, 3]]
+gene_chrom_lookup['gid'] = gene_chrom_lookup[3].apply(lambda x: x.split('.')[0])
+gene_chrom_lookup = gene_chrom_lookup.set_index('gid')[0]
+logging.info("gene chrom lookup table: {}".format(gene_chrom_lookup.shape))
 
-
+# output chroms used (Dec 29, 2020)
+output_chroms = outdir + "/chroms_{}.txt".format(name)
 
 ### ---- fixed after ----
 # ## Read in data 
@@ -116,11 +126,16 @@ logging.info('Done reading data')
 
 subsample_fraction = args.subsample_fraction
 subsample_times = args.subsample_times
+
+feature_subsample_fraction = args.feature_subsample_fraction
+feature_subsample_times = args.feature_subsample_times
+
 # subsampling 80% of cells from each modality
 for i in range(subsample_times):
     output_tag = ".{}".format(i)
 
     logging.info("Subsampling {}/{}".format(i+1, subsample_times))
+    # subset cells
     metas_sub, gxc_hvftrs_sub = subsampling(
                 mods_selected, settings, metas, gxc_hvftrs, p=subsample_fraction,
                 )
@@ -129,27 +144,33 @@ for i in range(subsample_times):
         cells_mod = metas_sub[mod].index.values
         np.save(fout, cells_mod)
 
-    # ## run SCF
-    pcX_all, cells_all = SCF_utils.core_scf_routine(mods_selected, features_selected, settings, 
-                                                    metas_sub, gxc_hvftrs_sub, # sub 
-                                                    ps, drop_npcs,
-                                                    cross_mod_distance_measure, knn, relaxation, n_cca,
-                                                    npc,
-                                                    output_pcX_all+output_tag, output_cells_all+output_tag,
-                                                    output_imputed_data_format+output_tag,
-                                                    ka_smooth=ka_smooth,
-                                                    save_knn=save_knn,
-                                                    output_knn_within=output_knn_within+output_tag,
-                                                    output_knn_across=output_knn_across+output_tag,
-                                                    )
-    logging.info('Done integration into a common PC space')
+    # subset features
+    for n_split in np.arange(feature_subsample_times):
+        # split features
+        gene_set_lookup, chroms_0, chroms_1 = cluster_cv_utils.split_genes(
+                                                        gene_chrom_lookup, 
+                                                        split_frac=feature_subsample_fraction, 
+                                                        report_chroms=True,
+                                                        )
+        gxc_hvftrs_sub_g0, gxc_hvftrs_sub_g1 = cluster_cv_utils.split_features_routine(mods_selected, settings, gxc_hvftrs_sub, gene_set_lookup)
+        # use g0; record using g0 
+        output_tag2 = ".f{}".format(n_split)
+        fout = (output_chroms+output_tag+output_tag2)
+        basic_utils.export_single_textcol(fout, chroms_0)
+        logging.info('Saved {}'.format(fout))
 
-
-    # not needed 
-    # df_summary = SCF_utils.clustering_umap_routine(pcX_all, cells_all, mods_selected, 
-    #                                                metas_sub, # sub
-    #                                                resolutions, k, 
-    #                                                umap_neighbors, min_dist, 
-    #                                                output_clst_and_umap+output_tag,
-    #                                               )
-    # logging.info('Done clustering and UMAP')
+        # ## run SCF
+        pcX_all, cells_all = SCF_utils.core_scf_routine(mods_selected, features_selected, settings, 
+                                                        metas_sub, gxc_hvftrs_sub_g0, # sub 
+                                                        ps, drop_npcs,
+                                                        cross_mod_distance_measure, knn, relaxation, n_cca,
+                                                        npc,
+                                                        output_pcX_all+output_tag+output_tag2, 
+                                                        output_cells_all+output_tag+output_tag2,
+                                                        output_imputed_data_format+output_tag+output_tag2,
+                                                        ka_smooth=ka_smooth,
+                                                        save_knn=save_knn,
+                                                        output_knn_within=output_knn_within+output_tag+output_tag2,
+                                                        output_knn_across=output_knn_across+output_tag+output_tag2,
+                                                        )
+        logging.info('Done integration into a common PC space')
