0f5a453bee1cdc4bfd315d49169e5aa11aab57f3
Fangming Xie <fmxie1993@gmail.com>
2021-04-27 10:34:06 -0700

    CLI update

--- scripts/SCF_main.py
+++ scripts/SCF_main.py
@@ -17,121 +17,181 @@ import pandas # To import dataset_metadata.csv
 import basic_utils
 import SCF_utils
 
-log = basic_utils.create_logger()
-
-
 # TODO:
 # -- Clean up command line argument descriptions
 # -- Figure out where to put default values
 # -- Fix __init__dataset.py rept
 # -- Proper error messages (replace assert() with helpful messages)
 # -- Simplify API to take care of redundant information
+# -- Make the assumed input structures explicit (./datasets/dataset_metadata.csv)
+
+log = basic_utils.create_logger()
+
+DESCRIPTION="""
+SCF is a computational tool to integrate single-cell transcriptome and epigenome datasets. 
+
+Contributors: Fangming Xie, Aditya Chandrasekar, Wayne I. Doyle, Ethan J. Armand, Eran Mukamel
+Contact: Eran Mukamel (emukamel@ucsd.edu)
+"""
 
 def create_parser():
     """
     """
     parser = argparse.ArgumentParser(prog="SingleCellFusion",
-                                     description="SCF is a computational tool to \
-                                                  integrate single-cell transcriptome \
-                                                  and epigenome datasets",
+                                     description=DESCRIPTION,
                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     
     ## ARGUMENTS DIRECTLY FED INTO SingleCellFusion CLI 
 
     # Name of settings
-
-    parser.add_argument("-n", "--name", metavar="NAME", type=str,
-                         help="Name of current configuration",
-                         default="test_scf_biccn2")
+    # where does it show up in the results?
+    parser.add_argument(
+        "-n", "--name", 
+        metavar="NAME", 
+        type=str,
+        default="test_scf_biccn2",
+        help="Name of current configuration",
+    )
 
     # Input/Output Dataset Settings
+    # what's required false/true?
+    parser.add_argument(
+        "-d", "--dataset", 
+        metavar="DATASET_DIR", 
+        type=str,
+        default="./datasets",
+        required=False,
+        help="Directory containing input gene x cell tables as h5ad",
+    )
 
-    parser.add_argument("-d", "--dataset", metavar="DATASET_DIR", type=str,
-                        help="Directory containing input gene x cell tables as h5ad",
-                        default="./datasets",
-                        required=False)
-
-    parser.add_argument("-m", "--mods", metavar="MODALITY", type=str,
-                        help="Modalities inside the data directory to be integrated, \
-                            entered as a space seperated list of filenames.",
-                        nargs="+",
-                        required=True)
+    parser.add_argument(
+        "-m", "--mods", 
+        metavar="MODALITY", 
+        type=str,
+        nargs="+",
+        required=True,
+        help="Modalities inside the data directory to be integrated, \
+            entered as a space seperated list of filenames.",
+    )
     
-    parser.add_argument("-f", "--features", metavar="FEATURE", type=str,
-                        help="Modalities to impute into, entered as a space-separated \
-                            list of filenames. The features of these modalities will \
-                            be the features kept in the output imputed data table.",
-                        nargs="+",
-                        required=True)
+    parser.add_argument(
+        "-f", "--features", 
+        metavar="FEATURE", 
+        type=str,
+        nargs="+",
+        required=True,
+        help="Modalities to impute into, entered as a space-separated \
+            list of filenames. The features of these modalities will \
+            be the features kept in the output imputed data table.",
+    )
     
-    parser.add_argument("-o", "--output", metavar="OUT_DIR", type=str,
-                        help="Directory to store output of SingleCellFusion",
-                        default="./results")
+    parser.add_argument(
+        "-o", "--output", 
+        metavar="OUT_DIR", 
+        type=str,
+        default="./results",
+        help="Directory to store output of SingleCellFusion",
+    )
 
     # Measures Within Each Modality
+    parser.add_argument(
+        "--num_pcs", 
+        metavar="PCs", 
+        type=int,
+        default=50,
+        help="Number of Principal Components to use for main function",
+    )
 
-    parser.add_argument("--num_pcs", metavar="PCs", type=int,
-                        help="Number of Principal Components to use for main function",
-                        default=50)
+    parser.add_argument(
+        "--ps", 
+        metavar="PS",
+        default=[0.9, 0.7, 0.1],
+        help="TODO: Specified list of PS for [mc, rna, atac] \
+              data respectively",
+    )
 
-    parser.add_argument("--ps", metavar="PS",
-                        help="TODO: Specified list of PS for [mc, rna, atac] \
-                              data respectively",
-                        default=[0.9, 0.7, 0.1])
 
+    parser.add_argument(
+        "--cross_mod_distance", 
+        metavar="DISTANCE_METRIC", 
+        type=str,
+        default="correlation",
+        help="Distance for comparisons across modalities",
+    ) #correlation or cca
 
-    parser.add_argument("--cross_mod_distance", metavar="DISTANCE_METRIC", type=str,
-                        help="Distance for comparisons across modalities",
-                        default="correlation") #correlation or cca
+    parser.add_argument(
+        "--nearest_neighbors", 
+        metavar="kNN", 
+        type=int,
+        default=20,
+        help="Number of nearest neighbors used to impute data",
+    )
 
-    parser.add_argument("--nearest_neighbors", metavar="kNN", type=int,
-                        help="Number of nearest neighbors used to impute data",
-                        default=20)
+    parser.add_argument(
+        "--relaxation", 
+        metavar="RELAXATION", 
+        type=int,
+        default=3,
+        help="A integer between 1 to infinity. It specifies how much the datasets should be \
+            assumed to be structurally similar. \
+            relaxation=1 enforces a hard limit that every cell receives equal number of nearest neighbors\
+            relaaxation=infinity approaches traditional kNN",
+    )
 
-    parser.add_argument("--relaxation", metavar="RELAXATION", type=int,
-                        help="Relaxation specifies how much the datasets should be \
-                            assumed to be structurally similar when running SCF. \
-                            More concretely, with Relaxation = 1, when running the \
-                            nearest neighbor algorithm, each sell has a hard limit on\
-                            the number of neighbors it can have (to avoid the hub cell\
-                            problem). As Relaxation approaches infinity, this limit is\
-                            relaxed, and the algorithm approaches traditional kNN",
-                        default=3)
-
-    parser.add_argument("--n_cca", metavar="NUMBER_CCA", type=int,
-                        help="TODO",
-                        default=0)
+    parser.add_argument(
+        "--n_cca", 
+        metavar="NUMBER_CCA", 
+        type=int,
+        default=0,
+        help="TODO",
+    )
 
     # Number of PCs
-
-    parser.add_argument("--n_dropped_pcs", metavar="NUM_DROPPED_PCS", type=list,
-                        help="Specified list of Principal Components to drop for \
-                              [mc, rna, atac] respectively",
-                        default=[0, 0, 0])
+    parser.add_argument(
+        "--n_dropped_pcs", 
+        metavar="NUM_DROPPED_PCS", 
+        type=list,
+        default=[0, 0, 0],
+        help="Specified list of Principal Components to drop for \
+                [mc, rna, atac] respectively",
+    )
    
     # Arguments for Clustering
-
-    parser.add_argument("-k", "--k_clusters", metavar="K_CLUSTERS", type=int,
-                        help="Number of Clusters for late clustering stage of SingleCellFusion",
-                        default=30)
+    parser.add_argument(
+        "-k", "--k_clusters", 
+        metavar="K_CLUSTERS", 
+        type=int,
+        default=30,
+        help="Number of Clusters for late clustering stage of SingleCellFusion",
+    )
    
-    parser.add_argument("--resolutions", metavar="RESOLUTIONS", type=list,
-                        help="Series of Resolutions to be used for Leiden Clustering Algorithm. \
-                              Should be given as list of resolutions, to be iterated through and used \
-                              from left to right.",
-                        default=[0.1, 0.2, 0.4, 0.8])
+    parser.add_argument(
+        "--resolutions", 
+        metavar="RESOLUTIONS", 
+        type=list,
+        default=[0.1, 0.2, 0.4, 0.8],
+        help="Series of Resolutions to be used for Leiden Clustering Algorithm. \
+                Should be given as list of resolutions, to be iterated through and used \
+                from left to right.",
+        )
 
     # Arguments for UMAP
-
     # TODO: get documentation for --n_neighbors & --min_dist respectively
-
-    parser.add_argument("-u", "--umap_neighbors", metavar="UMAP_NEIGHBORS", type=int,
-                        help="TODO: Number of Neighbors used for UMAP stage of SingleCellFusion",
-			default=60)
+    parser.add_argument(
+        "-u", "--umap_neighbors", 
+        metavar="UMAP_NEIGHBORS", 
+        type=int,
+		default=60,
+        help="TODO: Number of Neighbors used for UMAP stage of SingleCellFusion",
+    )
    
-    parser.add_argument("--min_distance", metavar="MIN_UMAP_DISTANCE", type=float,
-                        help="TODO: Minimum Distance bused be used for UMAP stage of SingleCellFusion",
-                        default=0.5)
+    parser.add_argument(
+        "--min_distance", 
+        metavar="MIN_UMAP_DISTANCE", 
+        type=float,
+        default=0.5,
+        help="TODO: Minimum Distance bused be used for UMAP stage of SingleCellFusion",
+        )
 
     return parser
 
@@ -148,7 +208,6 @@ if not os.path.isdir(outdir):
     os.makedirs(outdir)
 
 # output file names
-
 name = args.name
 
 output_pcX_all = outdir + '/pcX_all_{}.npy'.format(name)
@@ -159,7 +218,6 @@ output_cluster_centroids = outdir + '/centroids_{}.pkl'.format(name)
 output_figures = outdir + '/{}_{{}}.{{}}'.format(name)
 
 # Get dataset configuration
-
 settings = collections.OrderedDict()
 
 # Load in dataset metadata
@@ -172,7 +230,6 @@ Mod_info = collections.namedtuple('Mod_info', metadata.columns)
 for _, row in metadata.iterrows():
     sample_name = row['mod']
     settings[sample_name] = Mod_info(*row)
-    
 
 # Select modalities and features
 mods_selected = args.mods
@@ -181,11 +238,9 @@ features_selected = args.features
 for features_modality in features_selected:
     assert (features_modality in mods_selected)
 
-
 data_f = os.path.join(DATA_DIR, "{0}.h5ad")
 
 # Within modality
-
 assert(len(args.ps) == 3)
 assert(all(isinstance(ps, float) for ps in args.ps))
 assert(len(args.n_dropped_pcs) == 3)
@@ -216,7 +271,6 @@ resolutions = args.resolutions
 # umap
 umap_neighbors = args.umap_neighbors
 min_dist = args.min_distance
-
 
 # ## Read in data 
 logging.info('* Begin integration')
